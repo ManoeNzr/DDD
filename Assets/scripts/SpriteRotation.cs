2fcc5ccb4792d8a78e11e0140ea999d1aade1eec
NzrManoe <m.nezer2@rubika-edu.re>
2025-10-11 11:33:04 +0400

    system de piece casi complet

--- Assets/scripts/SpriteRotation.cs
+++ Assets/scripts/SpriteRotation.cs
@@ -1,22 +1,14 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class SpriteAnimator : MonoBehaviour
 {
-    public Transform player;
-    public float rotationSpeed = 5f; // pour lisser un peu la rotation
-
-    void Update()
+    void Start()
     {
-        // Direction complète vers le joueur (on ne bloque plus l’axe Y)
-        Vector3 direction = player.position - transform.position;
-
-        if (direction.sqrMagnitude > 0.001f)
-        {
-            // Calculer la rotation vers le joueur
-            Quaternion lookRotation = Quaternion.LookRotation(direction);
-
-            // Appliquer la rotation (smooth)
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
-        }
+        CoinManager manager = FindAnyObjectByType<CoinManager>();
+        if (manager != null)
+            manager.RegisterCoin(transform);
+        else
+            Debug.LogWarning("Aucun CoinManager trouvé dans la scène !");
     }
 }
